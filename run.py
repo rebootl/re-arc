dda9ca1de3b9d6bbf9569b3470c89b3bebf767e4
Cem Aydin <cem.aydin@gmx.ch>
2024-09-22 20:23:56 +0200

    add run script

---
+++ run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+#
+#
+
+from main import demo_generator, generate_dataset
+# demo_generator('00d62c1b')
+
+# demo_generator('4612dd53')
+# demo_generator('d5d6de2d')
+demo_generator('5117e062')
+
+# generate_dataset('data-2', 15, 1000, examples=['4612dd53'])
+# generate_dataset('data-2', 15, 1000, examples=['d631b094'])
+# generate_dataset('data-3', 15, 1000, examples=['4612dd53', 'd631b094'])
+# generate_dataset('data-6', 15, 10000, examples=['4612dd53', 'd631b094'])
+# generate_dataset('data-7', 15, 50_000, examples=['4612dd53', 'd631b094'])
