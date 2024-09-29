2b7ef19a7692e47d3a5f2ed71195fcd2ecaab324
Cem Aydin <cem.aydin@gmx.ch>
2024-09-29 21:33:21 +0200

    upd. run script

--- run.py
+++ run.py
@@ -4,14 +4,56 @@
 #
 
 from main import demo_generator, generate_dataset
-# demo_generator('00d62c1b')
 
-# demo_generator('4612dd53')
-# demo_generator('d5d6de2d')
-demo_generator('5117e062')
 
-# generate_dataset('data-2', 15, 1000, examples=['4612dd53'])
-# generate_dataset('data-2', 15, 1000, examples=['d631b094'])
-# generate_dataset('data-3', 15, 1000, examples=['4612dd53', 'd631b094'])
-# generate_dataset('data-6', 15, 10000, examples=['4612dd53', 'd631b094'])
-# generate_dataset('data-7', 15, 50_000, examples=['4612dd53', 'd631b094'])
+
+examples=[
+    'dbc1a6ce',
+    '2281f1f4',
+    'c1d99e64',
+    '623ea044',
+    '1190e5a7',
+    '5614dbcf',
+    '05269061',
+    '1c786137',
+    '2204b7a8',
+    '23581191', # 10
+    '8be77c9e',
+    '6d0aefbc',
+    '74dd1130',
+    '62c24649',
+    '6150a2bd',
+    '6fa7a44f',
+    '8d5021e8',
+    '0520fde7',
+    '46442a0e',
+    '1b2d62fb', # 20
+    '3428a4f5',
+    '42a50994',
+    '8f2ea7aa',
+    '7fe24cdd',
+    '85c4e7cd',
+    '8e5a5113',
+    '4c4377d9',
+    'a65b410d',
+    '5168d44c',
+    'a9f96cdd', # 30
+    '9172f3a0',
+    '67a423a3',
+    'db3e9e38',
+    '9dfd6313',
+
+    # '4612dd53',
+    # 'd631b094',
+    # 'd5d6de2d',
+    # '5117e062',
+    # '7fe24cdd',
+    # '228f6490',
+    # '3428a4f5',
+    # '0dfd9992', # OK
+]
+
+# for e in examples:
+#     demo_generator(e)
+
+# generate_dataset('data-8', 15, 1, examples)
